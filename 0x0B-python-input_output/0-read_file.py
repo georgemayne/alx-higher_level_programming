56bdca0b335a23c4aa7916d8b0f28af3d62876f0
georgemayne <georgeegyirmayne07@gmail.com>
2023-09-16 18:13:30 -0700

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" Program that read file """
+"""
+Contains the read_file function
+"""
 
 
 def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end='')
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
