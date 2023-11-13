cf873bcc16c3d3201de4c4af497f08b55424b68b
georgemayne <georgeegyirmayne07@gmail.com>
2023-11-13 14:07:49 -0800

    JavaScript WarmUp

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let g = 0; g < lang.length; g++) {
-	console.log(lang[g]);
+for (const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	console.log(lang);
 }
