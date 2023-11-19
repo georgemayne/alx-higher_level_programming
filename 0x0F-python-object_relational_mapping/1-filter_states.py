81d443f94dec902c4c88ad3eff56c01bfa61ef74
georgemayne <georgeegyirmayne07@gmail.com>
2023-11-19 13:21:23 -0800

    Python ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Lists all states from the datahase hbtn_0e_0_usa."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = con.cursor()
+    c.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    query_rows = c.fetchall()
+    for row in query_rows:
+        print(row)
+    c.close()
+    con.close()
