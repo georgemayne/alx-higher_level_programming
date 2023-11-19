62ffef95a9a5aa50f34dd168104a9d162665e101
georgemayne <georgeegyirmayne07@gmail.com>
2023-11-19 13:52:18 -0800

    Python ORM

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""displays all values in the states table of hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = con.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    query_rows = c.fetchall()
+    for row in query_rows:
+        print(row)
+    c.close()
+    con.close()
