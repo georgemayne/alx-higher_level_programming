b5dbfdb1c5678d185b6bec30322743f89a9be562
georgemayne <georgeegyirmayne07@gmail.com>
2023-11-24 15:28:34 -0800

    Python ORM

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,9 +12,10 @@ Base = declarative_base()
 
 class City(Base):
     """Represents city for a MySQL database.
-    Attributes: id (str): The city's id.
-        name (sqlalchemy.Integer): The city's name.
-        state_id (sqlalchemy.String): The city's state id.
+    Attributes:
+    id (str): The city's id.
+    name (sqlalchemy.Integer): The city's name.
+    state_id (sqlalchemy.String): The city's state id.
     """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
